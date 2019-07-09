406716d391f91963992c84002d94ba4659ef6671
Brock Smith <brocksmith225@gatech.edu>
2019-07-08 20:01:20 -0400

    Now all tags with the same text are selected when one is selected

--- js/skills.js
+++ js/skills.js
@@ -1,7 +1,20 @@
 $(".skill").click(function(){
     if ($(this).hasClass("active")) {
         $(this).removeClass("active");
+        tag = $(this).html();
+        $(".skill").each(function() {
+            if (tag == $(this).html()) {
+                $(this).removeClass("active");
+            }
+        });
     } else {
         $(this).addClass("active");
+        tag = $(this).html();
+        console.log(tag)
+        $(".skill").each(function() {
+            if (tag == $(this).html()) {
+                $(this).addClass("active");
+            }
+        });
     }
 });
